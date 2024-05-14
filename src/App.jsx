393dd57b5b966dd7a709ddc366cc4c3cf1c2d31a
Crosscut <crosscutcode@gmail.com>
2024-05-14 19:17:42 -0400

    Basic vite setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import "./styles/style.css";
+
+function App() {
+  return (
+    <div className="app">
+      <Header></Header>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
