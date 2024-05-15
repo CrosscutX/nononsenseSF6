65e17da064ba12caa442281e26a81323b34a6829
Crosscut <crosscutcode@gmail.com>
2024-05-15 14:03:38 -0400

    Intro completed

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-
 import "../../styles/home.css";
 import Intro from "./Intro";
 import Hero from "./Hero";
+import Author from "./Author";
 export default function Home() {
   return (
     <main className="home">
       <Hero />
       <Intro />
+      <Author />
     </main>
   );
 }
--- src/components/home/Intro.jsx
+++ src/components/home/Intro.jsx
@@ -7,6 +7,23 @@ export default function Intro() {
         Fighting game is something so great.
         <footer className="author">-Tokido</footer>
       </blockquote>
+      <p>
+        Fighting games are one of the most compelling genres of games out there.
+        There are few games that can match the thrill and excitement that comes
+        from a tense back and forth tournament set where any small mistake could
+        mean disaster. Fighting games have allowed me to connect with friends
+        and legitimately allowed me to learn about myself and improve
+        personally, in ways that help me in my day to day life. Sadly, because
+        of the intimidating nature of the genre, many people abandon the journey
+        before they get a chance to realize just captivating it can be.
+      </p>
+      <p>
+        My goal is to help bridge that gap for less experienced fighting game
+        players, and help them gain a deeper understanding of the theory behind
+        Street Fighter and hopefully allow them to gain a deeper appreciation
+        for the genre, and hopefully gain more confidence inside the game and in
+        their day to day lives.
+      </p>
     </div>
   );
 }
