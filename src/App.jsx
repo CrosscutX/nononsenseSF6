822ee4bcf503874c3b759b1620ee390d819e8e5a
Crosscut <crosscutcode@gmail.com>
2024-05-14 21:09:33 -0400

    Header issue fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import React from "react";
 import Header from "./components/Header";
+import Home from "./components/Home";
+import Lessons from "./components/lessons/Lessons";
+import Resources from "./components/Resources";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./styles/style.css";
 
 function App() {
   return (
     <div className="app">
       <Header></Header>
-      <h1>Hello World</h1>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/lessons" element={<Lessons />} />
+        <Route path="/resources" element={<Resources />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/header.css";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <nav>
+        <div className="header-left">
+          <Link to="/">
+            <span className="no">NO</span> NONSENSE
+          </Link>
+        </div>
+        <div className="header-right">
+          <Link to="/lessons">Lessons</Link>
+          <Link to="/resources">Resources</Link>
+        </div>
+      </nav>
+    </header>
+  );
 }
