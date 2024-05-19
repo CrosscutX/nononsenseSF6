4ebfdce9bee0d97ed40e62bcf3ae93223c26b39f
Crosscut <crosscutcode@gmail.com>
2024-05-19 14:55:49 -0400

    Execution done

--- src/components/lessons/Introduction/Execution.jsx
+++ src/components/lessons/Introduction/Execution.jsx
@@ -12,6 +12,62 @@ export default function Execution() {
         Fighter experience, So I figured I'd write a short article about it.
       </p>
       <h3>Take Your Time</h3>
+      <p>
+        Rome wasn't built in a day, and neither will your execution. So take
+        your time and don't expect to be cranking out crazy optimized combos in
+        your first week of playing fighting games. Take an iterative approach to
+        your execution. Start with easy concepts in training mode, master them
+        and use them in match, and then try to slowly build up the complexity.
+      </p>
+      <p>
+        I find a lot of new players try to really master their execution
+        instantly, going for huge combos they've seen pros do, or that they've
+        seen the combo trials. This is often completely unecessary for
+        beginners, and creates a lot of early frustration when they aren't
+        pulling off things that are really meant for much more advanced players.
+        So don't fall into this trap, keep it simple.
+      </p>
+      <p>
+        If you have any specific issues with execution, you'll likely have the
+        most success looking up youtube tutorials for whatever issue you're
+        having, or reaching out to the community through reddit/discord. The FGC
+        is generally very welcoming, don't be afraid to reach out!
+      </p>
+      <h3>On Modern Controls</h3>
+      <p>
+        Modern controls seems to be a pretty heated topic among community
+        members, so I'd like to be as clear as possible in what I'm about to
+        say. There's nothing morally wrong with playing modern as a newbie,
+        you're allowed to enjoy the video game in whatever way you'd like, you
+        paid for the damn thing after all. At the high level, I think there is
+        still some competitive merit for the control type. If you are a strong
+        tournament player seeking to experiment with the control type, then by
+        all means this isn't directed at you.
+      </p>
+      <p>
+        The people that I believe shouldn't be sticking to modern, are the
+        newbies that have decided they are dedicated and would really like to
+        learn fighting games. Execution is part of what makes the genre so
+        awesome, it's not something you should really run away from. Being able
+        to pull off complicated sequences is one of the most fun things that
+        fighting games have to offer, especially when it comes to pulling it off
+        in match. To avoid classic, is to avoid that great feeling of mastering
+        something, and that's the type of stuff that really keeps you going long
+        term.
+      </p>
+      <p>
+        Street Fighter, is also not the only fighting game. It's my favorite,
+        but there's a whole sea of other games that exist. There's a good chance
+        that the best game for you isn't Street Fighter. If you decide to stick
+        with modern controls, you're going to find yourself completely
+        relearning your execution should you ever try to learn different games
+        somewhere down the line.
+      </p>
+      <p>
+        So really, if you do decide that you love this genre and would like to
+        put some time into it, then simplified controls will really only hurt
+        you long term. You should be aware of that.
+      </p>
     </>
   );
 }
