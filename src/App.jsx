c06904daac7a654c58f4dbbe389072dd0cf96a9a
Crosscut <crosscutcode@gmail.com>
2024-05-20 17:18:24 -0400

    Scroll to top functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./components/home/Home";
@@ -17,31 +17,38 @@ import BeginnerPressure from "./components/lessons/Beginner/BeginnerPressure";
 import BeginnerCombos from "./components/lessons/Beginner/BeginnerCombos";
 import Resources from "./components/resources/Resources";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 import "./styles/style.css";
 import "./styles/lessons.css";
 
 function App() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="app">
       <Header />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/lessons" element={<LessonsLayout />}>
-          <Route index element={<Lessons />} />
-          <Route path="howthisworks" element={<HowThisWorks />} />
-          <Route path="aboutme" element={<AboutMe />} />
-          <Route path="mindset" element={<Mindset />} />
-          <Route path="characterselection" element={<CharacterSelection />} />
-          <Route path="execution" element={<Execution />} />
-          <Route path="hardware" element={<Hardware />} />
-          <Route path="beginnerneutral" element={<BeginnerNeutral />} />
-          <Route path="beginnerdefense" element={<BeginnerDefense />} />
-          <Route path="beginnerpoking" element={<BeginnerPoking />} />
-          <Route path="beginnerpressure" element={<BeginnerPressure />} />
-          <Route path="beginnercombos" element={<BeginnerCombos />} />
-        </Route>
-        <Route path="/resources" element={<Resources />} />
-      </Routes>
+      <ScrollToTop>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/lessons" element={<LessonsLayout />}>
+            <Route index element={<Lessons />} />
+            <Route path="howthisworks" element={<HowThisWorks />} />
+            <Route path="aboutme" element={<AboutMe />} />
+            <Route path="mindset" element={<Mindset />} />
+            <Route path="characterselection" element={<CharacterSelection />} />
+            <Route path="execution" element={<Execution />} />
+            <Route path="hardware" element={<Hardware />} />
+            <Route path="beginnerneutral" element={<BeginnerNeutral />} />
+            <Route path="beginnerdefense" element={<BeginnerDefense />} />
+            <Route path="beginnerpoking" element={<BeginnerPoking />} />
+            <Route path="beginnerpressure" element={<BeginnerPressure />} />
+            <Route path="beginnercombos" element={<BeginnerCombos />} />
+          </Route>
+          <Route path="/resources" element={<Resources />} />
+        </Routes>
+      </ScrollToTop>
       <Footer />
     </div>
   );
--- src/components/lessons/Beginner/BeginnerDefense.jsx
+++ src/components/lessons/Beginner/BeginnerDefense.jsx
@@ -4,6 +4,81 @@ export default function BeginnerDefense() {
   return (
     <>
       <h2>Beginner Defense</h2>
+      <p>You probably aren't blocking enough.</p>
+      <p>
+        That's it, you can close the article and go practice blocking. For real
+        it's that simple for most people.
+      </p>
+      <p>
+        To explain it further, blocking is an incredibly strong and low risk
+        tool in Street Fighter, it's very rare in my experience that blocking
+        too much puts people in a bad situation, 99% of the time it's blocking
+        too little that hurts weaker players. Since Street Fighter has so many
+        different options on defense, I think it's easy to get carried away
+        committing you options that aren't necessary, and thus many players
+        throw games away by overusing parry, dragon punch, throw, mash, ect.
+      </p>
+      <p>
+        Compared to the anime style games I've played, like Dragonball FighterZ
+        and Guilty Gear Strive, Street Fighter's guard is an incredibly potent
+        option. Plus frames on block is far more rare in SF6 than it is in those
+        games, most buttons don't give advantage unless you spend half your
+        drive gauge to drive rush cancel. Where compared to Dragonball, any
+        button that touches your guard likely means that you'll have to guess
+        correctly on a mixup. Despite the relative safety of guarding in SF6,
+        new players seem allergic to using it.
+      </p>
+      <p>
+        Below the master level, blocking can essentially become a free win
+        action. As it'll convince weaker opponents to over commit on offense as
+        they get frustrated by not being able to land buttons on you. When the
+        opponents are needing to invite risk in order to win, and you aren't,
+        you're typically putting yourself in a much easier spot to win matches.
+      </p>
+      <h3>Start To Understand Frame Data</h3>
+      <p>
+        Now all this blocking doesn't really matter if you let people walk all
+        over you despite you being at advantage(aka, being plus on block). Being
+        plus on block means you can act before the opponent. On average, buttons
+        in SF6 leave the defender at advantage, meaning it becomes their turn to
+        attack after they've blocked or normal or parried a move. Understanding
+        when it's your turn to fight back on block as a defender, is key to
+        understanding how Street Fighter actually functions.
+      </p>
+      <p>
+        Now this is unfortunately, not always true. There are some common
+        instances when the attacker actually retains advantage, I'll outline
+        some of them here, but remember that this is not all encompassing and
+        this will require you to consistently try out different things on, or
+        seek out frame data to understand the situations.
+      </p>
+      <h3>Common Advantagous Buttons</h3>
+      <h4>Jabs chained together</h4>
+      <h4>Some st. MPs</h4>
+      <h4>Lots of buttons after drive rush</h4>
+      <h4>Lots of buttons during burnout</h4>
+      <h4>Some special moves</h4>
+      <p>
+        On the flip side of this, there's also a large amount of moves that tend
+        to be punishable on block, meaning they are so minus that you can hit
+        the opponent after blocking their move. The rules here are probably even
+        more specific and will take some initial study, but a typical one that
+        you can apply is sweeping someone's sweep(although, even this has some
+        outliers with sweeps like Chun-Li's who is minus 7).
+      </p>
+      <h3>Don't get overwhelmed</h3>
+      <p>
+        You aren't going to develop a dense knowledge of frame data in a day,
+        just like execution, think of this as an ongoing iterative process.
+        Whenever a specific move is giving you issues and you don't understand
+        it, then go into training mode, turn on the frame data display, and
+        figure out what your options are against said move. The frame data
+        display tells you the +/- frames of every move on block, making this
+        sort of labbing much easier. After doing this consistently over the
+        course of a few months, you'll find one day that you've developed a
+        pretty strong understanding of the cast. Future you will thank you for
+        putting in the defensive work early.
+      </p>
     </>
   );
 }
