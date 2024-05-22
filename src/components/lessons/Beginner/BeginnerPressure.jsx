2ed7a05218bbfcd4ccac96b8a5566b3b9b149bcb
Crosscut <crosscutcode@gmail.com>
2024-05-21 20:58:33 -0400

    Pressure

--- src/components/lessons/Beginner/BeginnerPressure.jsx
+++ src/components/lessons/Beginner/BeginnerPressure.jsx
@@ -4,6 +4,83 @@ export default function BeginnerPressure() {
   return (
     <>
       <h2>Beginner Pressure</h2>
+      <p>
+        You did it, you stuffed the opponent's aggression, and now you've
+        secured a knockdown. So what now?
+      </p>
+      <h3>Oki</h3>
+      <p>
+        Oki is Japanese term that essentially translates to "wake up pressure".
+        There's tons of situations in Street Fighter where you get to continue
+        attacking the opponent after a combo. This is typically referred to as
+        "getting oki". Oki allows players to lockdown their opponents and force
+        guessing situations on them, often times leading to huge damage
+        conversions if the defender is wrong. It is your job as a player, to
+        experiment with knockdowns to see what oki you get. Also remember to
+        test drive rush, as it's necessary to get oki in certain scenarios.
+      </p>
+      <h3>Basics Of Pressure</h3>
+      <p>
+        Once you understand how to get your oki, now it's time to start exerting
+        pressure. Pressure in SF6 really comes down to 3 core options.
+      </p>
+      <h3>It All Starts With Meaty</h3>
+      <p>
+        A meaty is an attack that's meant to hit the earliest frames of an
+        opponent waking up. This is the concept that gives the attacker the
+        biggest advantage in wake up scenarios, because the attacker can
+        threaten the defender with high damage normals, while the defender
+        can't. Meatying is a simple concept, it pins the opponent in place and
+        prevents them from mashing on you. Get good at this concept and you'll
+        be able to control opponents waking up, forcing them to deal with your
+        offense.
+      </p>
+      <h3>Throws Are The Backbone</h3>
+      <p>
+        Throw is the core tool that really makes Street Fighter offense
+        function. Throw is a completely unreactable attack that beats opponents
+        who are guarding. Typically the only other way a character can open up
+        an opponent low blocking is by using an overhead, but this strategy is
+        fickle because an aware opponent does have time to react to most
+        overheads.
+      </p>
+      <p>
+        Throwing while giving you a nice little bit of damage, is primarily good
+        for one reason. Throw forces the opponent to act. If an opponent doesn't
+        tech, doesn't backdash, and doesn't jump, they will just die to your
+        repeated throws. Predicting when these options will be used, is when you
+        are likely to score big damage, even on strong opponents.
+      </p>
+      <h3>The Shimmy</h3>
+      <p>
+        While typically not taught to beginners, I think it's important for new
+        players to start considering shimmy. Shimmy completes the pressure
+        trifecta that allows you to open up any opponent, because it beats a
+        very common option select called "delay tech". I'm not going to get into
+        the weeds of delay tech yet, but just know that if opponents are teching
+        you often, shimmy is the go to option to punish them hard for daring to
+        tech.
+      </p>
+      <p>
+        Shimmy as a concept isn't very complicated, you simply need to walk to
+        the edge of the opponents throw range and walk back. Since throws aren't
+        reactable, the opponent needs to react to the threat of your movement,
+        so sometimes they'll guess wrong and tech. After you back away, the
+        opponent will be left wide open reaching at air, allowing you to land
+        your biggest punish.
+      </p>
+      <p>
+        Shimmy also has the added benefit of naturally beating reversals. So
+        implementing shimmy means that'll you'll accidently beat reversals,
+        which is nice bonus for sure.
+      </p>
+      <p>
+        While there's much more we could talk about in terms of pressure, these
+        3 options are already enough to open up every single player on the
+        planet. No single option exists that can beat all 3. So mastering these
+        core pressure options can quite literally take you to evo, everything
+        else is supplementary.
+      </p>
     </>
   );
 }
